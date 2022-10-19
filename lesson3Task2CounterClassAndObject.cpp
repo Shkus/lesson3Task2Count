3309faee04490e8e763d0b42a547038428c57510
virtualshkus <virtualshkus@gmail.com>
2022-10-19 23:35:19 +0500

    changeCodeFirstRedact

--- lesson3Task2CounterClassAndObject.cpp
+++ lesson3Task2CounterClassAndObject.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -7,64 +8,49 @@ private:
     int startCounter;
 
 public:
-    Counter(int startCounter) {};
+    Counter(int startCounter) { this->startCounter = startCounter; };
 
-    void setNum(int startCounter1) { startCounter = startCounter1; }
-
-    Counter(std::string positive01, std::string positive02,
-        std::string negative01, std::string negative02, std::string answer0) {
-
-        if (positive01 == answer0 || positive02 == answer0) {
-
-            std::cout << "Введите начальное значение счётчика: ";
-            std::cin >> startCounter;
-            setNum(startCounter);
-            std::cout << std::endl;
-
-
-        }
-        else {
-
-            Counter(1);
-
-            std::cout << std::endl;
-        }
-    };
+    void setNum(int startCounter1) { this->startCounter = startCounter1; }
 
     int add() { return ++startCounter; };
 
     int dif() { return --startCounter; };
 
-    void equally() { std::cout << startCounter; };
+    int equally() { return startCounter; };
 };
 
 int main(int argc, char** argv) {
+
     setlocale(LC_ALL, "rus");
+
     Counter cont(1);
+
     std::cout
         << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
 
     std::string positive1 = "Да";
     std::string positive2 = "да";
-    std::string negative1 = "Нет";
-    std::string negative2 = "нет";
-    std::string positiveEn1 = "Yes";
-    std::string positiveEn2 = "yes";
-    std::string negativeEn1 = "No";
-    std::string negativeEn2 = "no";
-    positive1 = positiveEn1;
-    positive2 = positiveEn2;
-    negative1 = negativeEn1;
-    negative2 = negativeEn2;
 
     std::string answer;
     std::cin >> answer;
 
-    //int startCounter;
+    int startCounter1;
+
+    if (positive1 == answer || positive2 == answer) {
+
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> startCounter1;
+        cont.setNum(startCounter1);
+        std::cout << std::endl;
+
+    }
+    else {
+
+        Counter cont(1);
+    }
+
 
     std::cout << std::endl;
-
-    Counter(positive1, positive2, negative1, negative2, answer);
 
     std::cout << "Команды управления: " << std::endl
         << "+ : увеличьте счётчик на 1" << std::endl
@@ -93,8 +79,9 @@ int main(int argc, char** argv) {
             cont.dif();
         }
         else if (command == equally) {
-            cont.equally();
+            std::cout << cont.equally();
         }
 
     } while (command != completion);
 }
+
